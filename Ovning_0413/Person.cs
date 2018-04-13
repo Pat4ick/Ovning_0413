b17a7ffd8cd50363a39f443d434dcb5ba51a008c
Patrick <lmpatrick.frank@gmail.com>
2018-04-13 09:14:42 +0200

    Initial

---
+++ Ovning_0413/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning_0413
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Person
+
+    {
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        private string _fName;
+
+        public string FName
+        {
+            get { return _fName; }
+            set { _fName = value; }
+        }
+
+        private string _lName;
+
+        public string LName
+        {
+            get { return _lName; }
+            set { _lName = value; }
+        }
+
+        private float _height;
+
+        public float Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        private float _weight;
+
+        public float Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+
+
+    }
+}
--- Ovning_0413/Program.cs
+++ Ovning_0413/Program.cs
@@ -10,6 +10,16 @@ namespace Ovning_0413
     {
         static void Main(string[] args)
         {
+            /*
+             * 1. Setting value of the private field 
+             */
+
+            var person = new Person();
+            person.Age = 49;
+            Console.WriteLine("Yes, I can access the private fields indireclty by using the public properties\n" +
+                $"I have set the age to \"49\" as an example and here i read the value back again - {person.Age}\n");
+
+
         }
     }
 }
