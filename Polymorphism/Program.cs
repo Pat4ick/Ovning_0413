c30941f1b977274450fc23751ff347062597a13c
Patrick <lmpatrick.frank@gmail.com>
2018-04-18 10:53:23 +0200

    Complete

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -20,6 +20,19 @@ namespace Polymorphism
                 new SlowResponseError(),
                 new WrongChoiseError(),
 
+                // 11. F: Varför är polymorfism viktigt att behärska?
+                // Man kan enkelt ändra beteendet hos en basklass genom override and overload. Det förenklar expansionen av kodbasen.
+
+                // 12. F: Hur kan polymorfism förändra och förbättra kod via en bra struktur?  
+                // Genom att polymorfism kan man skapa en mer flexibel klasstruktur då arbetsuppgifterna kan fördelas mer logiskt mellan klasserna
+                
+                // 13. F: Vad är det för en skillnad på en Abstrakt klass och ett Interface?
+                // Ett interface är lite som en abstrakt klass där alla medlemmar är abstrakta och
+                // där det dessutom är krav på att alla metoder och fält ska implementeras i den implementerade klassen.
+                // Man kan implementera fler interface, men bara en abstrakt klass. En abstrakt klass kan innehålla konstruktorer.
+
+
+
             };
 
             foreach(UserError error in errors)
